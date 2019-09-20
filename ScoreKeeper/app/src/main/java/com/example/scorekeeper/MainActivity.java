d2269464fcc0e12af83c380b763e73937c4b6269
zygimantas97 <47702538+zygimantas97@users.noreply.github.com>
2019-09-20 12:00:00 +0300

    SignIn ir SignUp layout'ai

--- ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.scorekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onButtonClickSignUp(View view){
+        Intent intentObj = new Intent(this, SignUpActivity.class);
+        startActivity(intentObj);
+    }
+    public void onButtonClickSignIn(View view){
+
     }
 }
